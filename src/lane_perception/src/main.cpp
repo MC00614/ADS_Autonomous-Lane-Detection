e44943f870e629e11ab47da08267382ad5870b11
mczb <12190588@inha.edu>
2024-01-03 23:37:55 +0100

    Modify tap size

--- src/lane_perception/src/main.cpp
+++ src/lane_perception/src/main.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<LanePerception>());
-  rclcpp::shutdown();
-  return 0;
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<LanePerception>());
+	rclcpp::shutdown();
+	return 0;
 }
